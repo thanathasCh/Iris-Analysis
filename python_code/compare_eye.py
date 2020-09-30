0a148decf5a00094eea2b819d0eb448b79d30432
u6128302 <u6128302@au.edu>
2020-09-30 14:28:52 +0700

    Change something

--- python_code/compare_eye.py
+++ python_code/compare_eye.py
@@ -4,6 +4,8 @@ from skimage.measure import compare_ssim
 import imutils
 import os
 
+coordinate = []
+
 def blur(img):
     # kernel = np.ones((5,5),np.float32)/25
     # dst = cv2.filter2D(img,-1,kernel)
@@ -11,6 +13,7 @@ def blur(img):
     return dst
 
 def compare(img1, img2, i):
+    global coordinate
     (score, diff) = compare_ssim(img1, img2, full=True)
     diff = (diff * 255).astype("uint8")
     
@@ -26,6 +29,7 @@ def compare(img1, img2, i):
         if w < 50 and h < 50:
             cv2.rectangle(diff, (x, y), (x + w, y + h), (0, 0, 255), 1)
             cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 0, 255), 1)
+            coordinate.append((x, y, i))
 
     cv2.imwrite("result/diff_detected" + str(i) + ".png", diff)
     # cv2.imwrite("thresh.png", thresh)
@@ -33,6 +37,7 @@ def compare(img1, img2, i):
     cv2.imwrite("result/right_detected" + str(i) + ".png", img2)
 
 def start():
+    global coordinate
     PATH = "clean_datasets/right/"
     counter = 1
     for i in os.listdir(PATH):
@@ -51,3 +56,16 @@ def start():
 
         compare(clean_gray, diseased_gray, counter)
         counter += 1
+
+    coor = cv2.imread("result/eye.png")
+    coor = cv2.resize(coor, (300, 300), interpolation = cv2.INTER_AREA)
+    for i in coordinate:
+        color = (0,255,255)
+        if i[2] == 1:
+            color = (0,0,255)
+        if i[2] == 2:
+            color = (0,255,0)
+        if i[2] == 3:
+            color = (255,0,0)
+        cv2.rectangle(coor, (i[0], i[1]), (i[0] + 5, i[1] + 5), color, 1)
+    cv2.imwrite("result/coor.png", coor)
